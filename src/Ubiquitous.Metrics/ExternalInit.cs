dde0e6487d19cc9ffbabcd6a4b4372f1cf663607
Alexey Zimarev <alex@zimarev.com>
2021-03-09 11:00:43 +0100

    Support more SDKs

---
+++ src/Ubiquitous.Metrics/ExternalInit.cs
@@ -0,0 +1,9 @@
+// ReSharper disable EmptyNamespace
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Runtime.CompilerServices {
+    [ExcludeFromCodeCoverage, DebuggerNonUserCode]
+    static class IsExternalInit { }
+}
