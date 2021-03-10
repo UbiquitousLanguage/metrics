6607a1241efaf3d71a9210de2832cf15463dfa8e
Alexey Zimarev <alex@zimarev.com>
2021-03-10 12:07:49 +0100

    Remove unused reference and relax the version constraint

--- src/Ubiquitous.Metrics.Dogstatsd/StatsdGauge.cs
+++ src/Ubiquitous.Metrics.Dogstatsd/StatsdGauge.cs
@@ -4,7 +4,6 @@ namespace Ubiquitous.Metrics.Dogstatsd {
     class StatsdGauge : StatsdMetric, IGaugeMetric {
         internal StatsdGauge(MetricDefinition metricDefinition) : base(metricDefinition) { }
 
-        public void Set(double value, string[]? labels)
-            => DogStatsd.Gauge(MetricName, value, tags: FormTags(labels));
+        public void Set(double value, string[]? labels) => DogStatsd.Gauge(MetricName, value, tags: FormTags(labels));
     }
 }
